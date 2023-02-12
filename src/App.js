7b2291daadd3a4e5fac1fcee21dc06dc97a377dd
tahabisginsoftware <tahabisginsoftware@gmail.com>
2023-02-12 12:47:39 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
-import logo from './logo.svg';
+import logo from './logo512.png';
 import './App.css';
+import { useState } from 'react';
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function App() {
+  const [password, setPassword] = useState('');
+
+  const genPw = () => {
+    const possibleCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+';
+    let password = '';
+    for(let i = 0; i < 16; i++) {
+      password += possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+    }
+    setPassword(password);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <div>
+        <h2>Welcome to pwgen!</h2>
+        <p className='pwT'><p>Password:</p>{password} 
+        {password !== "" && (
+        <button 
+        className='copyB'
+        onClick={() => {navigator.clipboard.writeText(password)}}> 
+        <CopyToClipboard
+        text= {password}
+        onCopy={() => alert("Copied to Clipboard!")}>
+          <span>Copy</span>
+        </CopyToClipboard></button>
+        )}
         </p>
+        <button 
+        className='genPWB'
+        onClick={genPw}
+        style={{width: 200, height: 50, marginBottom:25}}
+        >Generate new Password.</button>
+        </div>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://tahabisginsoftware.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Made by Taha Bisgin.
         </a>
       </header>
     </div>
